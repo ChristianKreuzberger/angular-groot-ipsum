b6be2a2560b04980074c442f4a3ac24bf275708e
Christian Kreuzberger <s4ko88@gmail.com>
2017-04-06 19:11:54 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Groot Ipsum
+This repo contains a very basic text generator that is only capable of writing "I am Groot" in various languages.
+
+Demo: [https://todo](Demo)
+
+## Languages
+If you want to submit more languages or texts, please have a look at [data/groot.json]() and submit a pull request.
+
+## Copyright
+Groot is copyright by ...
+
+This repo is under the WTFPL License. 
+
---
+++ js/groot.js
@@ -0,0 +1,146 @@
+"use strict";
+
+/**
+ * I am Groot
+ */
+var groot = angular.module('groot', [
+    'ui.bootstrap',
+    'ngResource',
+    'ngAnimate',
+    'toaster'
+]);
+
+
+/**
+ * I am Groot
+ */
+groot.factory('GrootService',
+    ["$resource", function ($resource) {
+        return $resource('data/groot.json', {}, {});
+    }]
+);
+
+
+/**
+ * I am Groot. I am Groot. I am Groot!
+ */
+groot.controller('GrootController', [
+    "$scope", "GrootService", "toaster", "$uibModal",
+    function($scope, GrootService, toaster, $uibModal) {
+    var vm = this;
+
+    /**
+     * I am Groot
+     * @type {string}
+     */
+    vm.groot = "";
+
+    /**
+     * I am Groot
+     * @type {string}
+     */
+    vm.currentLanguage = "en";
+
+    /**
+     * I am Groot
+     * @type {number}
+     */
+    vm.numParagraphs = 3;
+
+    /**
+     * I am Groot
+     * @type {number}
+     */
+    vm.numSentences = 25;
+
+    /**
+     * I am Grooooooooooot
+     */
+    GrootService.get().$promise.then(
+        function success(response) {
+            var groot = response.groot;
+            vm.grootLanguages = Object.keys(groot);
+            vm.grootTexts = groot;
+
+            initWatchGroup();
+        }
+    );
+
+    var initWatchGroup = function() {
+        $scope.$watchGroup(['vm.numParagraphs', 'vm.numSentences', 'vm.currentLanguage'],
+            function () {
+                // I am Groot
+                if (String(vm.numParagraphs).toLowerCase() == "groot"
+                    && String(vm.numSentences).toLowerCase() == "i am") {
+                    // todo: open modal dialog with groot
+                    console.log('I am groot');
+                    $uibModal.open({
+                        animation: true,
+                        templateUrl: 'youtube.html',
+                    });
+
+                    return;
+                }
+
+                // I am Groot
+                if (!parseInt(vm.numParagraphs) || !parseInt(vm.numSentences) || vm.numParagraphs < 0 || vm.numSentences < 0) {
+                    toaster.pop('error', "I am Groot!!!");
+                } else {
+                    vm.onGroot();
+                }
+            });
+    };
+
+
+    var getRandomNumber = function(max) {
+        var random = Math.ceil(1000*Math.random()) % max;
+        return random;
+    };
+
+    /**
+     * I am Groot
+     * @returns {string}
+     */
+    var generateGroot = function() {
+        var groot = "";
+
+        var currentLanguage = "";
+
+        // I am Groot
+        if (vm.currentLanguage != "") {
+            currentLanguage = vm.currentLanguage;
+        }
+
+        // I am Groot
+        for (var i = 0; i < vm.numParagraphs; i++) {
+            // I am Groot
+            for (var j = 0; j < vm.numSentences; j++) {
+
+                // I am Groot
+                if (vm.currentLanguage == null) {
+                    currentLanguage = vm.grootLanguages[
+                        getRandomNumber(vm.grootLanguages.length)
+                        ];
+                }
+
+                groot += vm.grootTexts[currentLanguage][
+                    getRandomNumber(vm.grootTexts[currentLanguage].length)
+                    ];
+                groot += ". "; // I am Groot
+            }
+
+            groot += "\n\n"; // I am Groot
+        }
+
+        return groot;
+    };
+
+    /**
+     * I am Groot - I am Groot
+     */
+    vm.onGroot = function() {
+        vm.groot = generateGroot();
+    };
+
+
+}]);
