9bb2a0e23b6e2802481d017da929d52d0c4f006f
Harald Nezbeda <hn@nezhar.com>
2017-04-14 10:28:44 +0200

    Updates dependencies

--- node_modules/angular-ui-bootstrap/template/datepickerPopup/popup.html.js
+++ node_modules/angular-ui-bootstrap/template/datepickerPopup/popup.html.js
@@ -1,6 +1,6 @@
 angular.module("uib/template/datepickerPopup/popup.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("uib/template/datepickerPopup/popup.html",
-    "<ul class=\"uib-datepicker-popup dropdown-menu uib-position-measure\" dropdown-nested ng-if=\"isOpen\" ng-keydown=\"keydown($event)\" ng-click=\"$event.stopPropagation()\">\n" +
+    "<ul role=\"presentation\" class=\"uib-datepicker-popup dropdown-menu uib-position-measure\" dropdown-nested ng-if=\"isOpen\" ng-keydown=\"keydown($event)\" ng-click=\"$event.stopPropagation()\">\n" +
     "  <li ng-transclude></li>\n" +
     "  <li ng-if=\"showButtonBar\" class=\"uib-button-bar\">\n" +
     "    <span class=\"btn-group pull-left\">\n" +
